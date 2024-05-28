ef05a934394178ee377ee35d03c6a6f09296aec0
Jan van de Locht <jan@vandelocht.uk>
2024-05-28 23:32:10 +0200

    Modified script to also stage encrypted files.

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-grep --recursive --files-without-match -e "AGE" secrets | xargs -n 1 sops --encrypt --in-place
+# grep --recursive --files-without-match -e "AGE" secrets | xargs -n 1 sops --encrypt --in-place
+grep --recursive --files-without-match -e "AGE" secrets | xargs -n 1 /bin/sh -c 'sops --encrypt --in-place "$@"; git add "$@";' ''
