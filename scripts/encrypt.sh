c6d91b47699a26ca77e2d77e74e8c6e4b8b67eb7
Jan van de Locht <jan@vandelocht.uk>
2024-05-29 05:44:21 +0200

    Enhanced the encrypting script.

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-# grep --recursive --files-without-match -e "AGE" secrets | xargs -n 1 sops --encrypt --in-place
-grep --recursive --files-without-match -e "AGE" secrets | xargs -n 1 /bin/sh -c 'sops --encrypt --in-place "$@"; git add "$@";' ''
+grep --recursive --files-without-match -e "AGE" secrets | xargs -n 1 -r /bin/sh -c 'echo encrypting: "$@"; sops --encrypt --in-place "$@"; echo encrypted. Now staging: "$@"; git add "$@";' ''
