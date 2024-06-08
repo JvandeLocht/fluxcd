e7e3e8678680cb4450a393374445050660aeba55
Jan van de Locht <jan@vandelocht.uk>
2024-06-08 15:40:55 +0200

    Fixed formating of setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-k create namespace flux-system
+kubectl create namespace flux-system
 
-cat ~/.config/sops/age/keys.txt |                                    kubectl create secret generic sops-age \                           --namespace=flux-system \
-  --from-file=age.agekey=/dev/stdin
+cat ~/.config/sops/age/keys.txt | kubectl create secret generic sops-age \
+        --namespace=flux-system \
+        --from-file=age.agekey=/dev/stdin
 
-flux bootstrap github \                                              --owner=$GITHUB_USER \                                             --repository=fluxcd \
-  --branch=main \                                                    --path=./clusters/production \                                     --personal
+flux bootstrap github \
+        --owner=$GITHUB_USER \
+        --repository=fluxcd \
+        --branch=main \
+        --path=./clusters/production \
+        --personal
