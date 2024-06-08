245c6b971cc44d6f12ecf4cc4de9b31777e22c48
Jan van de Locht <jan@vandelocht.uk>
2024-06-08 13:55:31 +0200

    Added a script to setup flux.

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+k create namespace flux-system
+
+cat ~/.config/sops/age/keys.txt |                                    kubectl create secret generic sops-age \                           --namespace=flux-system \
+  --from-file=age.agekey=/dev/stdin
+
+flux bootstrap github \                                              --owner=$GITHUB_USER \                                             --repository=fluxcd \
+  --branch=main \                                                    --path=./clusters/production \                                     --personal
