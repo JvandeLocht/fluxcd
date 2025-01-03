efe304c24eeb929a0a482e3767bef64d9014433b
Jan van de Locht <jan@vandelocht.uk>
2025-01-03 13:55:00 +0100

    excluded helm releases from the encryption script

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grep -rL "BEGIN AGE ENCRYPTED FILE" . | xargs grep -l "apiVersion: v1" | xargs grep -l "kind: Secret" | xargs -n 1 -r /bin/sh -c 'echo encrypting: "$@"; sops --encrypt --in-place "$@"; echo encrypted. Now staging: "$@"; git add "$@";' ''
+grep -rL "BEGIN AGE ENCRYPTED FILE" . | xargs grep -L "HelmRelease" | xargs grep -l "kind: Secret" | xargs -n 1 -r /bin/sh -c 'echo encrypting: "$@"; sops --encrypt --in-place "$@"; echo encrypted. Now staging: "$@"; git add "$@";' ''
